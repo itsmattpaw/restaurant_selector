f8b11580157e7f9fe96160ae1b3104d7d17f4384
itsmattpaw <itsmattpaw@gmail.com>
2022-03-08 06:57:15 -0500

    init setup

---
+++ lib/restaurant_selector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "restaurant_selector/version"
+
+module RestaurantSelector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/restaurant_selector/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RestaurantSelector
+  VERSION = "0.1.0"
+end
